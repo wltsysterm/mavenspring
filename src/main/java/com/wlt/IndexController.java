4c78b4174eae059174711a8a4e4657663e79c39a
魏霖涛 <3263138624@qq.com>
2018-01-25 11:25:32 +0800

    本工程搭载jetty服务器插件，jetty启动命令配置：jetty:run,本工程启动后的访问url:http:localhost

---
+++ src/main/java/com/wlt/IndexController.java
@@ -0,0 +1,23 @@
+package com.wlt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 魏霖涛
+ * @since 2018/1/25 0025
+ */
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+    /**
+     * 调试springmvc
+     * @return
+     */
+    @RequestMapping("/test")
+    public String test(){
+        return "springmvc test success";
+    }
+}
